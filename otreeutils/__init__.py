247684c2bd983dd5d97d2c980200916ca1e37ade
Markus Konrad <markus.konrad@wzb.eu>
2018-04-25 13:17:19 +0200

    updated setup version bump

--- otreeutils/__init__.py
+++ otreeutils/__init__.py
@@ -1,4 +1,4 @@
 __title__ = 'otreeutils'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Markus Konrad'
 __license__ = 'Apache License 2.0'
--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ with open(os.path.join(here, 'README.md')) as f:
 setup(
     name=otreeutils.__title__,
     version=otreeutils.__version__,
-
+    description='A package with common oTree utilities',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url=GITHUB_URL,
     project_urls={
         'Bug Reports': GITHUB_URL + '/issues',
@@ -51,6 +51,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
